d7fa80e9a41f876dfff457020237d4ade146d414
NDDC NIGERIA <ob384@live.mdx.ac.uk>
2025-03-12 19:11:31 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+import './App.css'
+import Hero from './components/hero'
+import Navigation from './components/navigation'
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Navigation />
+      <Hero />
+    </>
+  )
+}
+
+export default App
